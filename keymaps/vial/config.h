d988604659acb28c771842ed0ee557ccac710352
Praise Jacob <iamfelixkyun@gmail.com>
2025-05-29 12:45:01 +0530

    modify config.h to make onboard led glow on keypress

--- keymaps/vial/config.h
+++ keymaps/vial/config.h
@@ -1,8 +1,13 @@
 /* keyboard uid */
 #define VIAL_KEYBOARD_UID {0xDC, 0x6D, 0x9C, 0x42, 0x53, 0x42, 0x75, 0x0F}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 5}
 
 /* default layer count */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+// double tap bootloader reset
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25
