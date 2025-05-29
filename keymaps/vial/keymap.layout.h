7f94a784250d556d326481ebb94a083bad28b231
Praise Jacob <iamfelixkyun@gmail.com>
2025-05-29 17:50:45 +0530

    fix led on slave and modify layout for minus ctrl and shift

--- keymaps/vial/keymap.layout.h
+++ keymaps/vial/keymap.layout.h
@@ -1,3 +1,5 @@
+#include "keycodes.h"
+#include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 #define PASS KC_TRNS
 
@@ -10,11 +12,11 @@ typedef enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LGUI,                                            KC_RALT,
-                                                    KC_SPC,                               KC_SPC,
-                                                            KC_BSPC,                KC_ENT
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+                                            KC_SPC,                                            KC_ENT,
+                                                    KC_BSPC,                               MO(_NUMBERS),
+                                                            KC_LGUI,                MO(_SYMBOLS)
     ),
 
     [_NUMBERS] = LAYOUT_split_3x6_3(
